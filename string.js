40b270f3197d41b5e027c7ca7c9f5cf972c64484
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2022-10-31 15:00:33 -0100

    commit

---
+++ string.js
@@ -0,0 +1,57 @@
+
+// function caracter(name){
+//     let name = "lisa"
+
+//     for(let count = 0; count < name.length; count++){
+//         console.log(name[count])
+//     }
+
+//     return name
+// }
+
+
+// function novo(){
+
+//     let name = "Olavo"
+
+//     for(let i in name){
+//         console.log(name[i])
+//     }
+
+// }
+
+
+function novo2(nome) {
+
+
+    for(let i in nome){
+        console.log(nome[i])
+    }
+}
+
+
+
+
+// function letras(name){
+
+//     const names = "Lisa";
+
+
+//     for (let i of names) {
+
+   
+//       name = i.split(" ");
+
+//     }
+
+//    return name
+// }
+
+
+
+// stringExemplo = "João da Silva Oliveira";
+// resultado = stringExemplo.split(" ");
+
+
+
+
