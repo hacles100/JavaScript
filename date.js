3d9ca0c2f41cdeef988a8f17dae120f0d5beb5fb
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2022-11-03 15:02:31 -0100

    commit

--- date.js
+++ date.js
@@ -64,6 +64,58 @@ function nhaData2(elemento){
 }
 
 
+// dia da semana e mes por extenso
+
+
+
+
+function nhaData3(elemento){
+
+    let data = new Date()
+
+    let dia = data.getDate()
+    let ano = data.getFullYear()
+
+    let mes = data.getMonth()
+
+    arrayMes = new Array();
+    arrayMes[0] = "Janeiro";
+    arrayMes[1] = "Fevereiro";
+    arrayMes[2] = "Março";      
+    arrayMes[3] = "Abril";
+    arrayMes[4] = "Maio";
+    arrayMes[5] = "Junho";
+    arrayMes[6] = "Julho";
+    arrayMes[7] = "Agosto";
+    arrayMes[8] = "Setembro";
+    arrayMes[9] = "Outubro";
+    arrayMes[10] = "Novembro";
+    arrayMes[11] = "Dezembro";
+
+   arrayDia = new Array();
+   arrayDia[0] = "Domingo";
+   arrayDia[1] = "Segunda-Feira";
+   arrayDia[2] = "Terça-Feira";
+   arrayDia[3] = "Quarta-Feira";
+   arrayDia[4] = "Quinta-Feira";
+   arrayDia[5] = "Sexta-Feira";
+   arrayDia[6] = "Sábado";
+
+    
+    if(elemento === '/'){
+    
+        let time = `${arrayDia[dia]}/${arrayMes[mes]}/${ano}`
+    
+        return time
+
+    }else {
+
+        let time = `${arrayDia[dia]}-${arrayMes[mes]}-${ano}`
+    
+        return time
+    }
+   
+}
 
 
 
