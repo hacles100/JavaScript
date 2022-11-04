bd17fcdad28131959666ee64123878d45c7821a3
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2022-11-04 14:33:20 -0100

    commit

--- main3.js
+++ main3.js
@@ -1,1 +1,10 @@
 document.getElementById("msg").innerHTML = "ola"
+
+document.getElementById("paragrafo").innerHTML = "Love eu te amo"
+
+document.getElementById("header").innerHTML = "Treinando para ser o melhor do mundo!"
+
+// document.getElementById("paragrafo").parentNode.innerHTML = "ola"
+
+document.title = "Love"
+
