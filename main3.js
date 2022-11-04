445735621955c8d51ffd2f7269a26603179b3fd2
haclesrocha200@gmail.com <71661551+hacles100@users.noreply.github.com>
2022-11-04 15:15:31 -0100

    funcao

--- main3.js
+++ main3.js
@@ -1,10 +1,35 @@
-document.getElementById("msg").innerHTML = "ola"
+// document.getElementById("msg").innerHTML = "ola"
 
 document.getElementById("paragrafo").innerHTML = "Love eu te amo"
 
 document.getElementById("header").innerHTML = "Treinando para ser o melhor do mundo!"
 
+
 // document.getElementById("paragrafo").parentNode.innerHTML = "ola"
 
 document.title = "Love"
+
+
+function nhaData(){
+
+    let data = new Date()
+
+    let dia = data.getDay()
+    let mes = data.getMonth() + 1
+    let ano = data.getFullYear()
+
+    let time = `${dia}/${mes}/${ano}` 
+
+    document.getElementById("data").innerHTML = time
+
+}
+
+function addTextDiv(name){
+
+    return document.getElementById("msg").innerHTML +=  name
+
+}
+
+
+
 
